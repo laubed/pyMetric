0a0bad085b6f9b4d13e4d4d8b353830b4879dac4
Daniel Laube <bitbucket@dlaube.de>
2016-05-04 11:15:15 +0200

    Added simple configuration file for database credentials

--- server/RatseMonitor.py
+++ server/RatseMonitor.py
@@ -4,11 +4,12 @@ import psycopg2
 from flask import Flask, jsonify, abort, request, make_response
 
 from ratselib.decorators import crossdomain
+from config import *
 
 app = Flask(__name__)
 
 
-database = psycopg2.connect(host="localhost", port=5432, user="dashboard", password="123456", database="metrics")
+database = psycopg2.connect(host=DATABASE_HOST, port=DATABASE_PORT, user=DATABASE_USER, password=DATABASE_PASS, database=DATABASE_NAME)
 cursor = database.cursor();
 cursor.execute("CREATE TABLE IF NOT EXISTS log_messages (Time INTEGER, Origin TEXT, Message TEXT, Type INTEGER);")
 database.commit();
