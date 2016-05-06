7485375ce3f5e95cad96025c4373a63a2d1073c5
Daniel Laube <bitbucket@dlaube.de>
2016-05-06 11:33:20 +0200

    Implemented MAX and MIN Operations on metric entries

--- pyMetricServer/handler/metric.py
+++ pyMetricServer/handler/metric.py
@@ -1,11 +1,52 @@
 import time
 
+import math
 from flask import request
 from pyMetricServer import app
 from pyMetricServer.system.database import database
 from werkzeug.exceptions import abort
-
 from pyMetricServer.system.decorators import crossdomain
+
+
+@app.route("/metric/api/v1.0/metric/<string:operation>/<string:key>/<int:fromtime>/<int:totime>")
+@crossdomain(origin="*")
+def get_metric(operation, fromtime, totime, key):
+    if operation == "MAX":
+        cursor = database.cursor()
+        cursor.execute("SELECT Id, Time, Origin, Key, Value FROM log_metric WHERE Time > %s AND Time < %s AND Key = %s ORDER BY TIME ASC", (fromtime, totime, key))
+        maxvalue = 0;
+        resentry = None
+
+        for row in cursor:
+            if row[4] >= maxvalue:
+                maxvalue = row[4]
+                resentry = row
+
+        if resentry != None:
+            return "{'Id': '%s', 'Time': '%s', 'Origin': '%s', 'Key': '%s', 'Value': '%s'}" % (str(resentry[0]), str(resentry[1]), str(resentry[2]), str(resentry[3]), str(resentry[4]))
+        else:
+            return "{}"
+
+    if operation == "MIN":
+        cursor = database.cursor()
+        cursor.execute(
+            "SELECT Id, Time, Origin, Key, Value FROM log_metric WHERE Time > %s AND Time < %s AND Key = %s ORDER BY TIME ASC",
+            (fromtime, totime, key))
+        minvalue = float("inf")
+        resentry = None
+
+        for row in cursor:
+            if row[4] <= minvalue:
+                minvalue = row[4]
+                resentry = row
+
+        if resentry != None:
+            return "{'Id': '%s', 'Time': '%s', 'Origin': '%s', 'Key': '%s', 'Value': '%s'}" % (
+            str(resentry[0]), str(resentry[1]), str(resentry[2]), str(resentry[3]), str(resentry[4]))
+        else:
+            return "{}"
+
+    return "{'message': 'Invalid operator'}"
 
 
 @app.route('/metric/api/v1.0/metric', methods=['POST'])
